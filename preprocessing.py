f65a412939611fea6ebe080cf3880b943be11f4f
Lüder Welz <lwelz@uni.osnabrueck.de>
2026-02-04 19:36:06 +0100

    working on cleanup

---
+++ preprocessing.py
@@ -0,0 +1,230 @@
+# =================================================================================================================================================
+# IMPORTS
+# =================================================================================================================================================
+import numpy as np
+import librosa
+import pywt
+import noisereduce as nr
+
+
+# =================================================================================================================================================
+# PADDING FUNCTIONS
+# =================================================================================================================================================
+
+def zero_pad(x, level):
+    N = len(x)
+    divisor = 2 ** level
+    pad_amount = (-N) % divisor
+    if pad_amount > 0:
+        x = np.concatenate([x, np.zeros(pad_amount)])
+    return x
+
+def reflect_pad(x, level):
+    N = len(x)
+    divisor = 2 ** level
+    pad_amount = (-N) % divisor
+    if pad_amount > 0:
+        x = np.pad(x, (0, pad_amount), mode='reflect')
+    return x
+
+def edge_pad(x, level):
+    N = len(x)
+    divisor = 2 ** level
+    pad_amount = (-N) % divisor
+    if pad_amount > 0:
+        x = np.pad(x, (0, pad_amount), mode='edge')
+    return x
+
+def wrap_pad(x, level):
+    N = len(x)
+    divisor = 2 ** level
+    pad_amount = (-N) % divisor
+    if pad_amount > 0:
+        x = np.pad(x, (0, pad_amount), mode='wrap')
+    return x
+
+# =================================================================================================================================================
+# STATIONARY WAVELET TRANSFORM FUNCTIONS
+# =================================================================================================================================================
+def swt_decompose(x, wavelet, level):
+    coeffs = pywt.swt(x, wavelet, level=level)   # Liste von (cA, cD)
+    return coeffs
+
+def swt_reconstruct(coeffs, wavelet):
+    x_hat = pywt.iswt(coeffs, wavelet)
+    return x_hat
+# =================================================================================================================================================
+# MFCC EXTRACTION FUNCTIONS
+# =================================================================================================================================================
+#exracts the mfccs and projects it into 1D for linear SVM
+def extract_mfcc_features_1D(x, sr, n_mfcc, n_fft, hop_length, n_mels):
+    mfccs = librosa.feature.mfcc(y=x, sr=sr, n_mfcc=n_mfcc, n_fft=n_fft, hop_length=hop_length, n_mels=n_mels)
+    mfcc_mean = np.mean(mfccs, axis=1)
+    mfcc_std = np.std(mfccs, axis=1)
+    feat = np.concatenate([mfcc_mean, mfcc_std])
+    return feat
+
+def extract_mfcc_features(x, sr, n_mfcc, n_fft, hop_length, n_mels):
+    mfccs = librosa.feature.mfcc(y=x, sr=sr, n_mfcc=n_mfcc, n_fft=n_fft, hop_length=hop_length, n_mels=n_mels)
+    return mfccs
+
+# =================================================================================================================================================
+# THRESHOLDING FUNCTIONS
+# =================================================================================================================================================
+
+def hard_threshold(coeffs, thresholds):
+    if len(coeffs) != len(thresholds):
+        raise ValueError("coeffs und thresholds müssen gleich lang sein")
+
+    new_coeffs = []
+    for (cA, cD), t in zip(coeffs, thresholds):
+        t = float(t)
+        cD = np.asarray(cD)
+        cD_new = cD * (np.abs(cD) >= t)   # hard
+        new_coeffs.append((cA, cD_new))   # cA unverändert
+    return new_coeffs
+
+def soft_threshold(coeffs, thresholds):
+    if len(coeffs) != len(thresholds):
+        raise ValueError("coeffs und thresholds müssen gleich lang sein")
+
+    new_coeffs = []
+    for (cA, cD), t in zip(coeffs, thresholds):
+        t = float(t)
+        cD = np.asarray(cD)
+        cD_new = np.sign(cD) * np.maximum(np.abs(cD) - t, 0.0)  # soft
+        new_coeffs.append((cA, cD_new))
+    return new_coeffs
+
+def garrote_threshold(coeffs, thresholds):
+    if len(coeffs) != len(thresholds):
+        raise ValueError("coeffs und thresholds müssen gleich lang sein")
+
+    new_coeffs = []
+    for (cA, cD), t in zip(coeffs, thresholds):
+        cD = np.asarray(cD)
+        t = float(t)
+
+        abs_cD = np.abs(cD)
+        mask = abs_cD > t
+
+        cD_new = np.zeros_like(cD)
+
+        # Garrote-Schrumpfung nur dort, wo |cD| > t
+        # (t^2 / cD^2) ist safe, weil mask -> cD != 0 an der Stelle
+        cD_new[mask] = cD[mask] * (1.0 - (t * t) / (cD[mask] * cD[mask]))
+
+        new_coeffs.append((cA, cD_new))
+
+    return new_coeffs
+
+# =================================================================================================================================================
+# THRESHOLDING RULE FUNCTIONS
+# =================================================================================================================================================
+def visushrink_threshold(coeffs):
+    threshold =[]
+    for (cA, cD) in coeffs:
+        sigma = np.median(np.abs(cD)) / 0.6745
+        N = len(cD)
+        t = sigma * np.sqrt(2 * np.log(N))
+        threshold.append(t)
+    return threshold
+
+# =================================================================================================================================================
+# SPECTRAL GATING FUNCTONS
+# =================================================================================================================================================
+def spectral_gating_denoise(x, sr, prop_decrease=1.0):
+    # x is signal, sr is sampling rate, pro decrease is how much noise to reduce (1.0 is full reduction)
+    reduced_noise = nr.reduce_noise(y=x, sr=sr, prop_decrease=prop_decrease)
+    return reduced_noise
+
+
+
+
+
+# =================================================================================================================================================
+# =================================================================================================================================================
+# =================================================================================================================================================
+# =================================================================================================================================================
+# MAIN FUNCTION
+# =================================================================================================================================================
+# =================================================================================================================================================
+# =================================================================================================================================================
+# =================================================================================================================================================
+def main_denoise(padding, x, level, wavelet, k_factors_method, use_debugging, threshold_method, threshold_rule):
+    #variables
+    new_coeffs = []
+    k = []
+    threshold =[]
+    #padding
+    if padding =="zero":
+        x= zero_pad(x, level)
+    elif padding =="reflect":
+        x= reflect_pad(x, level)
+    elif padding =="edge":
+        x= edge_pad(x, level)
+    elif padding =="wrap":
+        x= wrap_pad(x, level)
+    
+    #decompose with swt
+    coeffs = swt_decompose(x, wavelet, level)
+
+    #calculate k_factors
+    if k_factors_method == "default":
+        k_factors = np.linspace(3.0, 1.0, level)
+    elif k_factors_method == None:
+        k_factors = np.ones(level)
+    elif k_factors_method == "energy":
+        #calculate k_factors based on energy of detail coefficients
+        energies = []
+        for i in range(level):
+            cA, cD = coeffs[i]
+            energy = np.sum(cD ** 2)
+            energies.append(energy)
+        total_energy = np.sum(energies)
+        for energy in energies:
+            k = 1.0 - (energy / total_energy)
+            k_factors.append(k)
+    
+    #threshold rule
+    if threshold_rule == "visushrink":
+        threshold = visushrink_threshold(coeffs)
+    elif threshold_rule == "keine ahnung hier mehr":####################################################################
+        pass
+
+    #threshold the coeffs
+    if threshold_method == "hard":
+        new_coeffs = hard_threshold(coeffs, threshold)
+    elif threshold_method =="soft":
+        new_coeffs = soft_threshold(coeffs, threshold)
+    elif threshold_method == "garotte":
+        new_coeffs = garrote_threshold(coeffs, threshold)
+
+
+    #recosntruct
+    signal = swt_reconstruct(new_coeffs, wavelet)
+    return signal
+
+
+
+
+if __name__ == "__main__":
+#testing
+    #improtsa
+    import os
+    from debugging import plot_spec
+    #direction
+    base_dir = "C:/Users/Admin/Seafile/WhaleData" #base dir of desktop
+    species  = "Common_Dolphin"
+    file = "0984_5801400J"
+    folder = os.path.join(base_dir, species)
+    filename = os.path.join(folder, file + ".wav")
+    path = os.path.join(folder, filename)
+    data, sr = librosa.load(path, sr=None, mono=True)
+    plot_spec(data, sr)
+    signal = main_denoise(padding="zero", x=data, level=7, wavelet='db4', k_factors_method=None, use_debugging=False, threshold_method="soft", threshold_rule="visushrink")
+    plot_spec(signal, sr)
+
+
+    import matplotlib.pyplot as plt
+    plt.show()
